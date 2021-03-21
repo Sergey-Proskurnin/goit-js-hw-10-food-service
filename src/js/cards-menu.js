da00ee4530a306d062a3e8648db454d35b1b3cd4
Sergey-Proskurnin <proskurnin@ua.fm>
2021-03-22 00:30:51 +0200

    Add dark-theme without saving in localStorage

--- src/js/cards-menu.js
+++ src/js/cards-menu.js
@@ -1,6 +1,37 @@
 import menuCardsTpl from '../temlates/cards-menu.hbs';
 import menu from '../menu.json';
 
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  const body = document.querySelector('body')
+  const chekcBoxTheme = document.querySelector('#theme-switch-toggle')
+
+
+  
+  chekcBoxTheme.addEventListener('change', onCheckBoxInput)
+
+
+  function onCheckBoxInput (e) {
+      const theme = e.target.checked
+      if (theme === true) {
+        body.classList.add(Theme.DARK)
+      }
+      if (theme === false) {
+        body.classList.remove(Theme.DARK)
+      }
+      console.dir(theme);
+
+            // localStorage.setItem(Theme, theme)
+
+
+  }
+
+  console.log(chekcBoxTheme);
+
+
 const itemMenuCards = document.querySelector('.js-menu')
 const cardsMenu = createCardsMenu(menu)
 
