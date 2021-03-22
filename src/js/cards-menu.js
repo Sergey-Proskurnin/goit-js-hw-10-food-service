ce281c0b15400d0e5c2fbfc7d701a8c3b6577a92
Sergey-Proskurnin <proskurnin@ua.fm>
2021-03-23 01:48:19 +0200

    Add save theme to local storage

--- src/js/cards-menu.js
+++ src/js/cards-menu.js
@@ -1,44 +1,42 @@
 import menuCardsTpl from '../temlates/cards-menu.hbs';
 import menu from '../menu.json';
 
+const Key = 'keyTheme';
+
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const refs = {
+  body: document.querySelector('body'),
+  itemMenuCards: document.querySelector('.js-menu'),
+  chekcBox: document.querySelector('#theme-switch-toggle'),
+};
 
-  const body = document.querySelector('body')
-  const chekcBoxTheme = document.querySelector('#theme-switch-toggle')
+refs.chekcBox.addEventListener('change', onCheckBoxСhange);
+keepsChekcBoxСhange();
 
-
-  
-  chekcBoxTheme.addEventListener('change', onCheckBoxInput)
-
-
-  function onCheckBoxInput (e) {
-      const theme = e.target.checked
-      if (theme === true) {
-        body.classList.add(Theme.DARK)
-      }
-      if (theme === false) {
-        body.classList.remove(Theme.DARK)
-      }
-      console.dir(theme);
-
-            // localStorage.setItem(Theme, theme)
-
-
-  }
-
-  console.log(chekcBoxTheme);
-
-
-const itemMenuCards = document.querySelector('.js-menu')
-const cardsMenu = createCardsMenu(menu)
-
-itemMenuCards.insertAdjacentHTML( 'afterbegin', cardsMenu)
+const cardsMenu = createCardsMenu(menu);
+refs.itemMenuCards.insertAdjacentHTML('afterbegin', cardsMenu);
 
 function createCardsMenu(menu) {
-    return menuCardsTpl(menu)
-    
+  return menuCardsTpl(menu);
 }
 
+function onCheckBoxСhange(e) {
+  const theme = e.target.checked;
+  changeTheme(theme);
+  localStorage.setItem(Key, theme);
+}
+
+function keepsChekcBoxСhange() {
+  const savedТheme = JSON.parse(localStorage.getItem(Key));
+  refs.chekcBox.checked = savedТheme;
+  changeTheme(savedТheme);
+}
+
+function changeTheme(themes) {
+  themes
+    ? refs.body.classList.add(Theme.DARK)
+    : refs.body.classList.remove(Theme.DARK);
+}
